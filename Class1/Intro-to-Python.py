28022d37a85064b1477014f936cda21458b0d662
Irina Kukuyeva <ikukuyeva@gmail.com>
2019-01-10 19:39:49 -0800

    Class1: Add mention of writing 'pythonic' code and PEPs to better explain it

--- Class1/Intro-to-Python.py
+++ Class1/Intro-to-Python.py
@@ -5,7 +5,37 @@ get_ipython().run_line_magic('pdb', '')
 import os
 ORIG_DIR = os.getcwd()
 # # Introduction to Python
+
+# Python and [Python Enhancement Proposals (PEPs)](https://docs.python-guide.org/intro/community/) "describe changes to Python itself, or the standards around it".
+
+# [PEP 8](https://pep8.org/) = **The Style Guide for Python Code**
 # 
+
+# [PEP 20](https://www.python.org/dev/peps/pep-0020/) = **The Zen of Python**
+# ```
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
+# ```
+
+# Goal: Aim for `pythonic` code [[reference]](https://blog.startifact.com/posts/older/what-is-pythonic.html)
+
 # ## Python Basics
 #  Reference for further reading: https://automatetheboringstuff.com/chapter1/
 
@@ -183,6 +213,7 @@ def my_awesome_function(x):
 # - explanation of function arguments and return values
 # - example usage
 
+# [PEP 257](https://www.python.org/dev/peps/pep-0257/) for Docstring Conventions.
 
 
 my_awesome_function(3)
