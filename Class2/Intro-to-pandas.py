1b853cf349ea428ef53b7b6e647acd13565a9184
Irina Kukuyeva <ikukuyeva@gmail.com>
2019-01-20 13:17:34 -0800

    Class2: Add link to R package sqldf for comparison

--- Class2/Intro-to-pandas.py
+++ Class2/Intro-to-pandas.py
@@ -507,7 +507,7 @@ df[['Origin', 'ArrDelay', 'DepDelay']].groupby('Origin').apply(
 
 # ### `pandasql`
 # - [`pandasql`](https://pypi.org/project/pandasql/) gives you ability to run `SQL` queries on your `pandas` data set
-# - Similar to `sqldf` package in `R`
+# - Similar to [`sqldf`](https://cran.r-project.org/web/packages/sqldf/sqldf.pdf) package in `R`
 # - Uses `SQLite` syntax [reference](https://pypi.org/project/pandasql/)
 
 # In[319]:
